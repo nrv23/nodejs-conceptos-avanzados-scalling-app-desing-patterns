1cc7916ab468425fa91cf3fe5a249d97ab4a712b
nrv23 <navemen23@hotmail.com>
2026-06-01 01:38:44 -0600

    procesing csv file backend and frontend

--- streams/procesing-csv-files/frontend/index.js
+++ streams/procesing-csv-files/frontend/index.js
@@ -1,15 +1,76 @@
 // http-server sirve para levantar un servidor local para correr codigo en frontend
 
-
 const URL = 'http://localhost:5000';
+let abortController = new AbortController();
 
+const start = document.getElementById('start');
+const stopbtn = document.getElementById('stop');
+const cards = document.getElementById('cards');
+
+function parseChnunk() {
+    let buffer = "";
+    return new TransformStream({
+        transform(chunk, controller) {
+            buffer += chunk;
+            const lines = buffer.split('\n');
+            buffer = lines.pop();
+
+            for (const line of lines) {
+
+                try {
+                    controller.enqueue(JSON.parse(line));
+                } catch (error) {
+                    console.error("Failed to parse JSON data", error, line);
+                }
+            }
+        },
+
+        flush(controller) {
+
+            try {
+                controller.enqueue(JSON.parse(buffer));
+            } catch (error) {
+                console.error("Failed to parse JSON data during flush", error, buffer);
+            }
+        }
+    })
+}
 
 async function consumeWebStreamsAPI(signal) {
     const response = await fetch(URL, { signal });
 
     const readerObj = response.body
         .pipeThrough(new TextDecoderStream())
-        .pipeTo(
+        .pipeThrough(parseChnunk())
+
+    return readerObj;
+}
+
+function displayData(element) {
+    return new WritableStream({
+        write({ poster_path, original_title, revenue }) {
+
+            const article = `
+            <article>
+                <div class="text">
+                    <h2>${original_title}</h2>
+                    <h3>${revenue}</h3>
+                    <a href="${poster_path}">View Poster</a>
+                </div>
+            </article>
+            `;
+
+            element.innerHTML += article;
+        }
+    });
+
+}
+
+
+
+/*
+
+    .pipeTo(
             new WritableStream({
                 write(chunk) {
                     console.log("Reading   ", chunk);
@@ -17,12 +78,24 @@ async function consumeWebStreamsAPI(signal) {
             })
         );
 
-    return readerObj;
-}
 
-const abortController = new AbortController();
+        new WritableStream({
+            write(chunk) {
+                console.log("Reading   ", chunk);
+            }
+        })
 
-(async () => { // funcion anonima autollamada
+*/
+
+start.addEventListener('click', async e => {
+
+    //e.preventDefault();
     console.log("Ejecuc")
-    await consumeWebStreamsAPI(abortController.signal);
-})();
+    const readableStream = await consumeWebStreamsAPI(abortController.signal);
+    readableStream.pipeTo(displayData(cards));
+});
+
+stopbtn.addEventListener('click', e => {
+    abortController.abort();
+    abortController = new AbortController();
+});
